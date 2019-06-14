7b93270f243cd7251f35f73ef74867b1762d397a
Divya Sasidharan <hello@shortdiv.com>
2019-06-14 14:02:31 -0400

    update fetch calendar code

--- functions/fetch-calendar/fetch-calendar.js
+++ functions/fetch-calendar/fetch-calendar.js
@@ -1,5 +1,42 @@
+const { google } = require("googleapis");
+
 exports.handler = async event => {
   const params = event.queryStringParameters;
+  const { code } = JSON.parse(params);
+
+  let oAuth2Client;
+
+  try {
+    getAccessToken(code);
+  } catch (e) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true
+      },
+      body: JSON.stringify({
+        error: e.message
+      })
+    };
+  }
+
+  function getAccessToken(code) {
+    const { CLIENT_SECRET, CLIENT_ID, REDIRECT_URIS } = process.env;
+
+    oAuth2Client = new google.auth.OAuth2(
+      `${CLIENT_ID}`,
+      `${CLIENT_SECRET}`,
+      `${REDIRECT_URIS}`
+    );
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error("Error retrieving access token", err);
+      oAuth2Client.setCredentials(token);
+
+      return oAuth2Client;
+    });
+  }
+
   return {
     statusCode: 200,
     headers: {
@@ -8,6 +45,6 @@ exports.handler = async event => {
       "Cache-Control": "no-cache",
       "Content-Type": "text/html"
     },
-    body: JSON.stringify({ params })
+    body: JSON.stringify({ oAuth2Client })
   };
 };
